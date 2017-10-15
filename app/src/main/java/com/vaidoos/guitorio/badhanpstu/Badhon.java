44ccdae70f4a310eb46ed69834c18dde3e41d603
guitorioadar <guitorioadar@gmail.com>
2017-10-15 21:48:45 +0600

    Fifth Commit

---
+++ app/src/main/java/com/vaidoos/guitorio/badhanpstu/Badhon.java
@@ -0,0 +1,15 @@
+package com.vaidoos.guitorio.badhanpstu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Badhon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_badhon);
+
+        getSupportActionBar().setTitle("Badhan");
+    }
+}
---
+++ app/src/main/java/com/vaidoos/guitorio/badhanpstu/Comittee.java
@@ -0,0 +1,15 @@
+package com.vaidoos.guitorio.badhanpstu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Comittee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comittee);
+
+        getSupportActionBar().setTitle("Comittee");
+    }
+}
